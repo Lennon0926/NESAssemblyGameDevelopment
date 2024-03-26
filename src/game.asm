f8963dcb5d1dfd17a09116e742411e87cc757f51
Onnelle Lugo <onnelle.lugo@upr.edu>
2024-03-25 20:23:52 -0400

    Refactor sprite loading and palette initialization

--- src/game.asm
+++ src/game.asm
@@ -24,55 +24,21 @@
   STX PPUADDR
   LDX #$00
   STX PPUADDR
-  LDA #$0F ; black
+load_palettes:
+  LDA palettes,X
   STA PPUDATA
-  LDA #$19
-  STA PPUDATA
-  LDA #$09
-  STA PPUDATA
-  LDA #$0f
-  STA PPUDATA
+  INX
+  CPX #$04
+  BNE load_palettes
 
-  ; first sprite
-  LDA #$00
-  STA $0200 ; Y-coord of first sprite
-  LDA #$02
-  STA $0201 ; tile number of first sprite
-  LDA #$00
-  STA $0202 ; attributes of first sprite
-  LDA #$00
-  STA $0203 ; X-coord of first sprite
-
-  ; second sprite
-  LDA #$00
-  STA $0204 ; Y-coord of first sprite
-  LDA #$03
-  STA $0205 ; tile number of first sprite
-  LDA #$00
-  STA $0206 ; attributes of first sprite
-  LDA #$08
-  STA $0207 ; X-coord of first sprite
-
-  ; second sprite
-  LDA #$08
-  STA $0208 ; Y-coord of first sprite
-  LDA #$12
-  STA $0209 ; tile number of first sprite
-  LDA #$00
-  STA $020A ; attributes of first sprite
-  LDA #$00
-  STA $020B ; X-coord of first sprite
-
-  ; second sprite
-  LDA #$08
-  STA $020C ; Y-coord of first sprite
-  LDA #$13
-  STA $020D ; tile number of first sprite
-  LDA #$00
-  STA $020E ; attributes of first sprite
-  LDA #$08
-  STA $020F ; X-coord of first sprite
-
+  ; write sprite data
+  LDX #$00
+load_sprites:
+  LDA sprites,X
+  STA $0200,X
+  INX
+  CPX #$10       ; # Sprites x 4 bytes
+  BNE load_sprites
 
 vblankwait:       ; wait for another vblank before continuing
   BIT PPUSTATUS
@@ -89,6 +55,16 @@ forever:
 
 .segment "VECTORS"
 .addr nmi_handler, reset_handler, irq_handler
+
+.segment "RODATA"
+palettes:
+.byte $0F, $19, $09, $0f
+
+sprites:
+.byte $70, $02, $00, $80
+.byte $70, $03, $00, $88
+.byte $78, $12, $00, $80
+.byte $78, $13, $00, $88
 
 .segment "CHR"
 .incbin "sprites.chr"
